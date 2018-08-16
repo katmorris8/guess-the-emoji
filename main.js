4abc01e91debf9c00e663094441309a4ee5032d4
Kat Morris <kathryn.leigh.morris@gmail.com>
2018-08-15 21:34:02 -0400

    added emojis object and created logic for selecting an emoji

---
+++ main.js
@@ -0,0 +1,35 @@
+const emojis = {
+    femaleDetective: {
+        gender: 'female',
+        hairColor: 'dark',
+        skinColor: 'light',
+        holding: 'magnifying glass',
+        headCover: 'hat',
+        mustache: false,
+        eyeCover: false,
+        collar: true,
+    },
+
+};
+const emojiElement = document.querySelectorAll('.emoji');
+const femaleDetectiveElement = emojiElement[0].getAttribute('id');
+let selectedEmoji;
+
+// have computer randomly choose an emoji by id
+// get the object keys to the corresponding emoji
+
+// event listener for when questions are clicked
+// based on the question get the value of the question
+// compare the value to the key
+// prompt an answer using a modal box
+
+// click event for a start button that runs chooseEmoji()
+function chooseEmoji() {
+    const randomEmoji = Math.floor( Math.random() * Object.keys(emojis).length);
+    selectedEmoji = Object.keys(emojis)[randomEmoji];
+    return selectedEmoji; // key name
+}
+
+chooseEmoji();
+
+
