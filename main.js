ae15fd1a13bc3d4b8fb3259e0c4a35d5dbd0a003
Kat Morris <kathryn.leigh.morris@gmail.com>
2018-08-16 10:07:23 -0400

    fixed logic for selecting a random emoji

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const emojis = {
 };
 const emojiElement = document.querySelectorAll('.emoji');
 const femaleDetectiveElement = emojiElement[0].getAttribute('id');
-let selectedEmoji;
+let selectedEmoji; // selected emoji index
 
 // have computer randomly choose an emoji by id
 // get the object keys to the corresponding emoji
@@ -23,13 +23,15 @@ let selectedEmoji;
 // compare the value to the key
 // prompt an answer using a modal box
 
+
 // click event for a start button that runs chooseEmoji()
 function chooseEmoji() {
     const randomEmoji = Math.floor( Math.random() * Object.keys(emojis).length);
     selectedEmoji = Object.keys(emojis)[randomEmoji];
-    return selectedEmoji; // key name
 }
 
 chooseEmoji();
+console.log(emojis.femaleDetective); // properties and values
+console.log(emojis[selectedEmoji]); //properties and values of selected emoji
 
 
